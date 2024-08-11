90922032cbbd0f453a7053017ae2a4faec680e73
ParkJinWu <onu0311@gmail.com>
2024-08-11 21:53:11 +0900

    Auth 수정

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthController.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthController.java
@@ -1,34 +1,26 @@
 package com.sparta.msa_exam.auth;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.sparta.msa_exam.auth.dto.SignUpDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
 
-    @GetMapping("/auth/signIn")
-    //원래는 POST로 해야된다.
-    // ? : Wildcard type ➡️ 어떤 타입이든 받는다.
-    public ResponseEntity<?> createAuthToken(@RequestParam String user_id){
-        // ok ➡️ HTTP 200
-        // AuthService에서 받아온 accesToken값
-        // yser_id로 Token을 만들고, access_token 키값으로 묶는다.
-        return ResponseEntity.ok(new AuthResponse(authService.createAccessToken(user_id)));
+    //@GetMapping("/auth/signIn")
+
+    @PostMapping("/signUp")
+    public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto){
+        log.info("Received signUp request: {}", signUpDto);
+        return ResponseEntity.ok(authService.signUp(signUpDto));
+
     }
 
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    //Response
-    static class AuthResponse{
-        private String accessToken;
-    }
+
 }
--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthService.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthService.java
@@ -1,56 +1,53 @@
 package com.sparta.msa_exam.auth;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
+import com.sparta.msa_exam.auth.auth.User;
+import com.sparta.msa_exam.auth.dto.SignUpDto;
+import com.sparta.msa_exam.auth.jwt.JwtUtil;
+import com.sparta.msa_exam.auth.mapper.UserMapper;
+import com.sparta.msa_exam.auth.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.crypto.SecretKey;
-import java.util.Date;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthService {
 
-    @Value("${spring.application.name}")
-    private String issuer;
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
 
-    @Value("${service.jwt.access-expiration}")
-    private Long accessExpiration;
-
-    private final SecretKey secretKey;
-
-    /**
-     * AuthService 생성자.
-     * Base64 URL 인코딩된 비밀 키를 디코딩하여 HMAC-SHA 알고리즘에 적합한 SecretKey 객체를 생성합니다.
-     *
-     * @param secretKey Base64 URL 인코딩된 비밀 키
-     */
-    public AuthService(@Value("${service.jwt.secret-key}") String secretKey) {
-        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+    public AuthService(UserRepository userRepository,
+                       JwtUtil jwtUtil,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    /**
-     * 사용자 ID를 받아 JWT 액세스 토큰을 생성합니다.
-     *
-     * @param user_id 사용자 ID
-     * @return 생성된 JWT 액세스 토큰
-     */
-    public String createAccessToken(String user_id) {
-        return Jwts.builder()
-                // 사용자 ID를 클레임으로 설정
-                .claim("user_id", user_id)
-                //.claim("role", "ADMIN")
-                // JWT 발행자를 설정
-                .issuer(issuer)
-                // JWT 발행 시간을 현재 시간으로 설정
-                .issuedAt(new Date(System.currentTimeMillis()))
-                // JWT 만료 시간을 설정
-                .expiration(new Date(System.currentTimeMillis() + accessExpiration))
-                // SecretKey를 사용하여 HMAC-SHA512 알고리즘으로 서명
-                .signWith(secretKey, io.jsonwebtoken.SignatureAlgorithm.HS512)
-                // JWT 문자열로 컴팩트하게 변환
-                .compact();
-    }
 
+    public String signUp(SignUpDto signUpDto) {
+        // 사용자 이름이 이미 존재하는지 확인
+        if (userRepository.existsByUsername(signUpDto.getUsername())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists");
+        }
+
+        if(userRepository.existsByEmail(signUpDto.getEmail())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already exists");
+        }
+
+        // 비밀번호를 암호화
+        String encodedPassword = passwordEncoder.encode(signUpDto.getPassword());
+        //signUpDto.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        // DTO를 엔티티로 변환
+        User user = UserMapper.toEntity(signUpDto, encodedPassword);
+
+        // 사용자 저장
+        userRepository.save(user);
+
+
+
+        return signUpDto.getUsername();
+    }
 }

45af8d303922ee7d806a52f476bed214e39609bf
ParkJinWu <onu0311@gmail.com>
2024-08-11 14:27:31 +0900

    JwtUtil 추가

---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/jwt/JwtUtil.java
@@ -0,0 +1,64 @@
+package com.sparta.msa_exam.gateway.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+@Component
+public class JwtUtil {
+    public static final Logger logger = LoggerFactory.getLogger("JWT Log");
+
+    private final SecretKey secretKey;
+
+    // Header KEY 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // Token 식별자
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    public JwtUtil( @Value("${service.jwt.secret-key}") String secretKey) {
+        this.secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secretKey));
+    }
+
+    public String getJwtToken(HttpServletRequest request) {
+        String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+        if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build().parseSignedClaims(token).getPayload();
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+}
